e5c58caf21998eec2d3e5345ff08622308d8e04a
Vitor Barbosa <vitor.araujo@wildlifestudios.com>
2020-11-02 19:05:56 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"batler/xcode"
+
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+	projectPath := flag.String("p", "", "Project path to run batler")
+	flag.Parse()
+
+	config, err := fetchConfiguration(*projectPath)
+	if err != nil {
+		log.Fatalf("could not fetch configuration: %v", err)
+	}
+
+	client := xcode.NewClient(config.Workspace, config.Scheme, config.BuildDir,
+		xcode.WithClean(), xcode.WithBuild(), xcode.WithTest())
+
+	if err := client.Run(); err != nil {
+		log.Printf("could not run client: %v", err)
+	}
+}
+
+const configName = ".batler.yml"
+
+type Configuration struct {
+	Workspace string
+	Scheme string
+	BuildDir string `yaml:"build_dir"`
+}
+
+func fetchConfiguration(projectPath string) (*Configuration, error) {
+	configPath := filepath.Join(projectPath, configName)
+	if !fileExists(configPath) {
+		return nil, fmt.Errorf("configuration file does not exist at %v", configPath)
+	}
+
+	yamlFile, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not read file: %v", err)
+	}
+
+	c := Configuration{}
+	yaml.Unmarshal(yamlFile, &c)
+
+	c.Workspace = filepath.Join(projectPath, c.Workspace)
+	// TODO (vitor.araujo): also join projectPath with c.BuildDir
+
+	return &c, nil
+}
+
+func fileExists(filepath string) bool {
+	_, err := os.Stat(filepath)
+	return !os.IsNotExist(err)
+}
---
+++ xcode/client.go
@@ -0,0 +1,77 @@
+package xcode
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+type client struct {
+	workspace, scheme, buildDir string
+	cleanEnabled, buildEnabled, testEnabled bool
+}
+
+type Option func(*client)
+
+func NewClient(workspace, scheme, buildDir string, opts ...Option) *client {
+	c := &client{
+		workspace: workspace,
+		scheme: scheme,
+		buildDir: buildDir,
+	}
+
+	for _, opt := range opts {
+		opt(c)
+	}
+
+	return c
+}
+
+func WithClean() Option {
+	return func(c *client) {
+		c.cleanEnabled = true
+	}
+}
+
+func WithBuild() Option {
+	return func(c *client) {
+		c.buildEnabled = true
+	}
+}
+
+func WithTest() Option {
+	return func(c *client) {
+		c.testEnabled = true
+	}
+}
+
+func (c *client) Run() error {
+	args := []string{
+		"-workspace",
+		c.workspace,
+		"-scheme",
+		c.scheme,
+		"-derivedDataPath",
+		c.buildDir,
+	}
+
+	if c.cleanEnabled {
+		args = append(args, "clean")
+	}
+	if c.buildEnabled {
+		args = append(args, "build")
+	}
+	if c.testEnabled {
+		args = append(args, "test")
+	}
+
+	cmd := exec.Command("xcodebuild", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error running xcodebuild: %v", err)
+	}
+
+	return nil
+}
